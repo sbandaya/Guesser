771fedd1e1f2cdc5bb2cdaa0c4a34500b3746f03
sbandaya <samuelandaya239@gmail.com>
2026-04-29 11:21:08 -0700

    test 1

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -15,8 +15,50 @@ class GuesserTest : public ::testing::Test
 };
 
 // Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
+TEST(GuesserTest, exact_matching_distance)
 {
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+  Guesser object("ABC");
+  ASSERT_EQ( 0 , object.distance("ABC);
+}
+
+TEST(GuesserTest, lower_case_distance)
+{
+  Guesser object("ABC");
+  ASSERT_EQ( 3, object.distance("abc");
+}
+
+TEST(GuesserTest, empty_distance)
+{
+  Guesser object("ABC");
+  ASSERT_EQ( 3, object.distace("");
+}
+
+TEST(GuesserTest, large_guess_distance)
+{
+  Guesser object("ABC");
+  ASSERT_EQ( 3, object.distance("qwertyyuiptypou");
+}
+
+TEST(GuesserTest, numbers_special_chars_distance)
+{
+  Guesser object("123&$%");
+  ASSERT_EQ( 4, object.distance("123&*@");
+}
+
+TEST(GuesserTest, matching_at_end_distance)
+{
+  Guesser object("ZXCabc");
+  ASSERT_EQ( 3, object.distance("POIabc");
+}
+
+TEST(GuesserTest, large_guess_end_different_distance)
+{
+  Guesser object("ABCDE");
+  ASSERT_EQ( 5, object.distance("ABCDEfghijkl );
+}
+
+TEST(GuesserTest, guess_end_different_distance)
+{
+  Guesser object("ABCDE");
+  ASSERT_EQ( 1, object.distance("ABCDEf);
 }
